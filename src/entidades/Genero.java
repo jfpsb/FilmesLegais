cbcdcff5f933e82cf23f17f4969df1c129ed0a34
Anderson Silva <andsf@live.com>
2015-01-07 10:32:53 -0300

    Depedências do JPA removidas

--- src/entidades/Genero.java
+++ src/entidades/Genero.java
@@ -1,14 +1,7 @@
 package entidades;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-
-@Entity
 public class Genero {
 
-	@Id
-	@GeneratedValue
 	private int id;
 	
 	private String genero;
